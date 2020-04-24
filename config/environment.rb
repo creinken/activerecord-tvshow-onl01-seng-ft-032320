0679940a28fc7779bee4807d37b53499a95512df
Christopher <reinkendigital@gmail.com>
2020-04-24 12:28:57 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -6,6 +6,7 @@ Bundler.require
 
 Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "../lib/support", "*.rb")].each {|f| require f}
+# require_relative '../app/shows_controller'
 
 DB = ActiveRecord::Base.establish_connection({
   adapter: 'sqlite3',
